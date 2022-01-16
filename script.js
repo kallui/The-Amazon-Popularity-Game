d08c4651f85abc59b69d5b476199dfb8092308cd
Gordon Lim <klgordon@students.cs.ubc.ca>
2022-01-16 01:22:01 -0800

    Added css variables

---
+++ script.js
@@ -0,0 +1,4 @@
+function change_css(){
+  document.getElementById("h2").style.fontSize = "35px";
+  document.getElementById("panel").style.cssText = "height: 100vh; background-image: url('https://m.media-amazon.com/images/I/61go0qfA1iL._AC_SY879_.jpg'), url('https://brand.ubc.ca/files/2018/09/Logos_1_2CrestDownload_768px.jpg'); background-repeat: no-repeat, no-repeat; background-size: 50% 100%; background-position: left top, right top;";
+}
