0664f9b88a261603e06c769535ab88a0e0427255
Gordon Lim <klgordon@students.cs.ubc.ca>
2022-03-10 19:24:39 -0800

    Reorganized files and cleaned up duplicates

---
+++ Game Page/GamePage.js
@@ -0,0 +1,243 @@
+let item = {
+    name: 'Banana Phone',
+    numOfRatings: 15,
+    url: "https://m.media-amazon.com/images/I/61T7-sgbZOL._AC_SL1500_.jpg"
+}
+
+const itemArray = []
+
+itemArray[0] = {name: 'Banana Phone',
+numOfRatings: 20,
+url: "https://m.media-amazon.com/images/I/61T7-sgbZOL._AC_SL1500_.jpg"}
+
+itemArray[1] = {name: 'Mirror',
+numOfRatings: 10,
+url: "https://m.media-amazon.com/images/I/61go0qfA1iL._AC_SY879_.jpg"}
+
+itemArray[2] = {name: 'Bounty Paper Tower',
+numOfRatings: 8500,
+url: 'https://m.media-amazon.com/images/I/81RfQ85XJKL._AC_SX679_.jpg'}
+
+itemArray[3] = {name: 'Cottonelle Toilet Paper',
+numOfRatings: 59020,
+url: 'https://m.media-amazon.com/images/I/810ytwZbkJS._AC_SX522_.jpg'}
+
+itemArray[4] = {name: 'Bellroy Slim Wallet',
+numOfRatings: 10,
+url: 'https://m.media-amazon.com/images/I/61Vtv7hVkWL._AC_UX695_.jpg'}
+
+itemArray[5] = {name: 'Amazon Essential Socks',
+numOfRatings: 4230,
+url: 'https://m.media-amazon.com/images/I/91J-numyfaL._AC_UX679_.jpg'}
+
+itemArray[6] = {name: 'Dortala Sofa',
+numOfRatings: 2,
+url: 'https://m.media-amazon.com/images/I/71tuA60M0cL._AC_SX679_.jpg'}
+
+itemArray[7] = {name: 'Craftsmam Hammer',
+numOfRatings: 1380,
+url: 'https://m.media-amazon.com/images/I/61aKrv6NzBL._AC_SX522_.jpg'}
+
+itemArray[8] = {name: 'Diyife Key Lock Box',
+numOfRatings: 510,
+url: 'https://m.media-amazon.com/images/I/71Pmmi+bpCL._AC_SX679_.jpg'}
+
+itemArray[9] = {name: 'North Face Infant Hoodie',
+numOfRatings: 70,
+url: 'https://m.media-amazon.com/images/I/71AP5WxDd1L._AC_UX679_.jpg'}
+
+itemArray[10] = {name: 'Outward Dog Treat Puzzle',
+numOfRatings: 8060,
+url: 'https://m.media-amazon.com/images/I/81TxartXF7L._AC_SX522_.jpg'}
+
+itemArray[11] = {name: 'Atomic Habits Book',
+numOfRatings: 46720,
+url: 'https://images-na.ssl-images-amazon.com/images/I/515mvgd0pXL._SX329_BO1,204,203,200_.jpg'}
+
+itemArray[12] = {name: 'Wet n Wild Lipstick',
+numOfRatings: 5050,
+url: 'https://m.media-amazon.com/images/I/61ZXr0o3ceL._AC_SX679_.jpg'}
+
+itemArray[13] = {name: 'Ecolution PopCorn Popper',
+numOfRatings: 200,
+url: 'https://m.media-amazon.com/images/I/81aTLT1IstL._AC_SX679_.jpg'}
+
+itemArray[14] = {name: 'GYMAX Treadmill',
+numOfRatings: 1260,
+url: 'https://m.media-amazon.com/images/I/61VUyZ92OaL._AC_SX522_.jpg'}
+
+itemArray[15] = {name: 'Amazon Basics Dumbells',
+numOfRatings: 21560,
+url: 'https://m.media-amazon.com/images/I/81YvGI3D0HL._AC_SX522_.jpg'}
+
+itemArray[16] = {name: 'Kindle',
+numOfRatings: 3120,
+url: 'https://m.media-amazon.com/images/I/61Ww4abGclL._AC_SX679_.jpg'}
+
+itemArray[17] = {name: 'GOTRAX Electric Scooter',
+numOfRatings: 50,
+url: 'https://m.media-amazon.com/images/I/61TBcpBMmjL._AC_SX522_.jpg'}
+
+itemArray[18] = {name: 'ecobee SmartThermostat',
+numOfRatings: 460,
+url: 'https://m.media-amazon.com/images/I/511k68zeC9L._AC_SX679_.jpg'}
+
+itemArray[19] = {name: 'Pamray Winter Boots',
+numOfRatings: 500,
+url: 'https://m.media-amazon.com/images/I/71CX8UKHLML._AC_UY695_.jpg'}
+
+itemArray[20] = {name: 'Amazon Echo Dot',
+numOfRatings: 62780,
+url: 'https://m.media-amazon.com/images/I/6182S7MYC2L._AC_SX679_.jpg'}
+
+itemArray[21] = {name: 'Catit Vesper Tunnel',
+numOfRatings: 190,
+url: 'https://m.media-amazon.com/images/I/71k4GX5AqgL._AC_SX522_.jpg'}
+
+itemArray[22] = {name: 'Purina Dry Dog Food',
+numOfRatings: 220,
+url: 'https://m.media-amazon.com/images/I/71ZVsuojZ2L._AC_SX522_.jpg'}
+
+itemArray[23] = {name: 'Meijieluo Shoulder Backpack',
+numOfRatings: 3,
+url: 'https://m.media-amazon.com/images/I/71e+nkZGaDL._AC_SX679_.jpg'}
+
+itemArray[24] = {name: 'singled Alexa Light Bulb',
+numOfRatings: 730,
+url: 'https://m.media-amazon.com/images/I/61stI5yEhZL._AC_SX679_.jpg'}
+
+itemArray[20] = {name: 'Christmas Tree Bag',
+numOfRatings: 50,
+url: 'https://m.media-amazon.com/images/I/81o6IyNcmUL._AC_SX679_.jpg'}
+
+itemArray[25] = {name: 'Torani Vanilla Syrup',
+numOfRatings: 980,
+url: 'https://m.media-amazon.com/images/I/31AGJ4qhIZL._AC_.jpg'}
+
+itemArray[26] = {name: 'ClubHouse Food Colouring',
+numOfRatings: 180,
+url: 'https://m.media-amazon.com/images/I/811g3l0JzDL._AC_SY879_.jpg'}
+
+itemArray[27] = {name: 'Yupik Roasted Cashew Nuts',
+numOfRatings: 160,
+url: 'https://m.media-amazon.com/images/I/91Bu8mncl0L._AC_SX679_.jpg'}
+
+itemArray[28] = {name: 'Surge Extension Cord',
+numOfRatings: 5090,
+url: 'https://m.media-amazon.com/images/I/61-8le1iwGS._AC_SX679_.jpg'}
+
+itemArray[29] = {name: 'Nicolas Cage Pillow',
+numOfRatings: 480,
+url: 'https://m.media-amazon.com/images/I/51SdwYh9x0L._AC_.jpg'}
+
+itemArray[30] = {name: 'Finger Covers',
+numOfRatings: 40,
+url: 'https://m.media-amazon.com/images/I/61RNk5OhabL._AC_SX679_.jpg'}
+
+itemArray[31] = {name: 'Frog Eye Mask',
+numOfRatings: 9,
+url: 'https://m.media-amazon.com/images/I/71NAE2Rjc9L._AC_SL1500_.jpg'}
+
+itemArray[32] = {name: 'Jasgood Nylon Belt',
+numOfRatings: 5830,
+url: 'https://m.media-amazon.com/images/I/71yUiV0VcDL._AC_SL1500_.jpg'}
+
+itemArray[33] = {name: 'Champion Beanie',
+numOfRatings: 1100,
+url: 'https://m.media-amazon.com/images/I/81NYfZQ2LiL._AC_UX679_.jpg'}
+
+itemArray[34] = {name: 'Marvel Avengers Pin',
+numOfRatings: 80,
+url: 'https://m.media-amazon.com/images/I/910VwU4bj9L._AC_SY879_.jpg'}
+
+itemArray[35] = {name: 'Vicks VapoRub',
+numOfRatings: 1190,
+url: 'https://m.media-amazon.com/images/I/81JmaY1HSEL._AC_SL1500_.jpg'}
+
+itemArray[36] = {name: 'Jamieson Laboratories Zinc',
+numOfRatings: 220,
+url: 'https://m.media-amazon.com/images/I/51pQRc-SmKL._AC_SL1000_.jpg'}
+
+itemArray[37] = {name: 'Tempered Glass Weighing Scale',
+numOfRatings: 780,
+url: 'https://m.media-amazon.com/images/I/61r7mOLzZML._AC_SL1500_.jpg'}
+
+itemArray[38] = {name: 'Black Face Mask',
+numOfRatings: 800,
+url: 'https://m.media-amazon.com/images/I/61+3W9Mr6eL._AC_SL1500_.jpg'}
+
+itemArray[39] = {name: 'Ceenwes Mens Grooming Kit',
+numOfRatings: 680,
+url: 'https://m.media-amazon.com/images/I/71s77UxmluL._AC_SL1500_.jpg'}
+
+itemArray[40] = {name: 'CARECJ Massage Gun',
+numOfRatings: 2150,
+url: 'https://m.media-amazon.com/images/I/61v2EDdcd4L._AC_SL1500_.jpg'}
+
+itemArray[41] = {name: 'Vicks Humidifier',
+numOfRatings: 2520,
+url: 'https://m.media-amazon.com/images/I/61BC140BMJL._AC_SX679_.jpg'}
+
+itemArray[42] = {name: 'Snow Joe Snow Shovel',
+numOfRatings: 15820,
+url: 'https://m.media-amazon.com/images/I/71LvoaGgGpL._AC_SL1500_.jpg'}
+
+itemArray[43] = {name: 'Impact Canopy Tent',
+numOfRatings: 130,
+url: 'https://m.media-amazon.com/images/I/41-nBmo4weL._AC_SL1001_.jpg'}
+
+itemArray[44] = {name: 'Perky-Pet Hummingbird Feeder',
+numOfRatings: 3530,
+url: 'https://m.media-amazon.com/images/I/81LjkP8PisL._AC_SL1500_.jpg'}
+
+itemArray[45] = {name: 'Dog Raincoat',
+numOfRatings: 2020,
+url: 'https://m.media-amazon.com/images/I/61RDOfGviFL._AC_SL1500_.jpg'}
+
+itemArray[46] = {name: 'Electric Snow Thrower',
+numOfRatings: 1780,
+url: 'https://m.media-amazon.com/images/I/81b6FoaE9nL._AC_SL1500_.jpg'}
+
+itemArray[47] = {name: 'Moen Garbage Disposer',
+numOfRatings: 9380,
+url: 'https://m.media-amazon.com/images/I/51oudbV-gPL._AC_SL1000_.jpg'}
+
+itemArray[48] = {name: 'Protection Rolled Gutter Guard',
+numOfRatings: 40,
+url: 'https://m.media-amazon.com/images/I/718HN7YfZ6S._AC_SL1500_.jpg'}
+
+itemArray[49] = {name: 'Mind Reader Umbrella Rack',
+numOfRatings: 1660,
+url: 'https://m.media-amazon.com/images/I/81iDxgRIHOL._AC_SL1500_.jpg'}
+
+itemArray[50] = {name: 'AmazonBasics Light-Weight Microfiber Sheet Set',
+numOfRatings: 256470,
+url: 'https://m.media-amazon.com/images/I/61P+L6VO57L._AC_SL1500_.jpg'}
+
+
+
+
+//Returns true if "item1 > item2" OR "Item 1 is more popular than Item 2"
+let counter = 2
+
+function compareTwoItems(clicked) {
+    
+    if (clicked == 1) {
+        counter = counter + 1
+        document.documentElement.style
+    .setProperty('--item2URL', "url(" + itemArray[counter].url + ")");
+    } else {
+        counter = counter + 1
+        document.documentElement.style
+        .setProperty('--item1URL', '' + "url(" + itemArray[counter].url + ")");
+    }
+}
+
+
+
+
+function change(int) {
+    if (int == 1) {
+
+    }
+}
